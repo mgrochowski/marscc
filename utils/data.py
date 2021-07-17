f8e795c2a3d61ef90ae3dca1f7076beaf911e198
Marek <grochu@is.umk.pl>
2021-07-17 09:52:39 +0200

    Add feature: save sampled images ans labels

--- utils/data.py
+++ utils/data.py
@@ -11,6 +11,7 @@ from pathlib import Path
 import urllib
 
 rgb_map = {
+       'background' : [255, 255, 255],
        'cone' :  [190, 190, 255],
        'crater' :  [115, 178, 115]
    }
@@ -21,6 +22,12 @@ label_map = {
       'crater' : 2
     }
 
+label_list = [
+    'background',
+    'cone',
+    'crater'
+    ]
+
 def image_to_labelmap(x, rgb_map=rgb_map, label_map=label_map):
     
     h, w, c = x.shape
@@ -30,17 +37,20 @@ def image_to_labelmap(x, rgb_map=rgb_map, label_map=label_map):
       
     return y
 
-def sample_image(x, y, out_size=(300, 300), max_zoom=3.0):
-    # size = ( width, height )
-    
-    
+def sample_image(x, y, out_size=(300, 300), max_zoom=1.0):
+
     height, width = x.shape[0] , x.shape[1]
-    
+    print('Input size: %dx%d' % (height, width))
+
     # draw position
     dx, dy = np.random.randint(0, width), np.random.randint(0, height)
     
     # draw size
-    zoom = 1.0 + np.random.rand() * (max_zoom - 1.0)
+    if max_zoom != 1.0:
+        zoom = 1.0 + np.random.rand() * (max_zoom - 1.0)
+    else:
+        zoom = max_zoom
+
     print('zoom', zoom)
     
     pw, ph = int(out_size[0] * zoom), int(out_size[1] * zoom)    
@@ -49,8 +59,10 @@ def sample_image(x, y, out_size=(300, 300), max_zoom=3.0):
     
     dx = dx - pw // 2
     dy = dy - pw // 2
+
     print('Check borders')
     print('Init', [dx, dy, dx+pw, dy + ph])
+
     if dx < 0: dx = 0
     if dy < 0: dy = 0
     if dx + pw > width: dx = width - pw
@@ -62,21 +74,21 @@ def sample_image(x, y, out_size=(300, 300), max_zoom=3.0):
     sy = y[dy:dy+ph, dx:dx+pw]
     
     # scale
-    sx = cv2.resize(sx, out_size, interpolation = cv2.INTER_AREA)
-    sy = cv2.resize(sy, out_size)
+    sx = cv2.resize(sx, out_size, interpolation = cv2.INTER_NEAREST)
+    sy = cv2.resize(sy, out_size, interpolation=cv2.INTER_NEAREST)
     return sx, sy
 
-
-channels = 1  # ilosc kanalow wejsciowych
-classes = 3  # ilosc klas
-# input_img_path = 'THEMIS.png'
-# target_img_path = 'masks.png'
-input_img_path = 'CTX_basemap_1.png'
-target_img_path = 'mask_1.png'
+#
+# channels = 1  # ilosc kanalow wejsciowych
+# classes = 3  # ilosc klas
+# # input_img_path = 'THEMIS.png'
+# # target_img_path = 'masks.png'
+# input_img_path = 'CTX_basemap_1.png'
+# target_img_path = 'mask_1.png'
 
 
 # funkcja tworzy zbiór treningowy obrazów składający się z 'n' obrazow o wymiarach 'nx' na 'ny'
-def _build_samples(sample_count, train_x, train_y, image_size=INPUT_SIZE, **kwargs):
+def build_samples(sample_count, train_x, train_y, image_size=INPUT_SIZE, channels=1, classes=3, **kwargs):
 
     nx, ny = image_size
     images = np.zeros((sample_count, nx, ny, channels))
@@ -87,12 +99,10 @@ def _build_samples(sample_count, train_x, train_y, image_size=INPUT_SIZE, **kwar
     for i in range(sample_count):
         image, mask = sample_image(train_x_norm, train_y, out_size=image_size, **kwargs)
         images[i, ..., 0] = image
-        labels[i, mask == 0, 0] = 1
-        labels[i, mask == 1, 1] = 1
-        labels[i, mask == 2, 2] = 1
+        for c in range(classes):
+            labels[i, mask == c, c] = 1
 
     return images, labels
-
 
 
 def download_data(target_dir='./data'):
@@ -113,7 +123,26 @@ def download_data(target_dir='./data'):
         print('Download ERROR')
 
 
-def load_data(sample_per_image=100, validation_split=0.25, file_path='data/train_files.txt', **kwargs):
+def save_samples(save_dir, images, labels):
+
+    if not Path(save_dir).exists():
+        Path(save_dir).mkdir(parents=True)
+
+    k = 0
+
+    for image, label in zip(images, labels):
+        cv2.imwrite(str(Path(save_dir).joinpath('%05d_image.png' % k)), image * 255.0)
+
+        for i, l in enumerate(label_list):
+            mask_img = label.copy()
+            mask_img[label[:, :, i] == 1.0, :] = rgb_map[l]
+
+        cv2.imwrite(str(Path(save_dir).joinpath('%05d_mask.png' % k)), mask_img)
+        k = k + 1
+
+
+def load_data(sample_per_image=100, validation_split=0.25, file_path='data/train_files.txt',
+              save_dir = None, **kwargs):
 
     if not Path(file_path).exists():
         download_data()
@@ -128,7 +157,7 @@ def load_data(sample_per_image=100, validation_split=0.25, file_path='data/train
             # print(line)
             files.append( ( str(data_dir.joinpath(x)) for x in line.split() ) )
 
-    train_samples, val_samples  = [], []
+    train_images, train_masks, val_images, val_masks  = [], [], [], []
 
     for image_path, mask_path in files:
 
@@ -148,11 +177,26 @@ def load_data(sample_per_image=100, validation_split=0.25, file_path='data/train
         train_x = input_img[:split, :]
         train_y = labels[:split, :]
 
-        train_samples.append(_build_samples(train_count, train_x, train_y, **kwargs))
-        val_samples.append(_build_samples(val_count,   val_x,   val_y,   **kwargs))
+        images, masks = build_samples(train_count, train_x, train_y, **kwargs)
+        train_images.append(images)
+        train_masks.append(masks)
 
-    return train_samples, val_samples
-    #
+        images, masks = build_samples(val_count, val_x, val_y, **kwargs)
+        val_images.append(images)
+        val_masks.append(masks)
+
+        train_images = np.concatenate(train_images, axis=0)
+        train_masks = np.concatenate(train_masks, axis=0)
+        val_images = np.concatenate(val_images, axis=0)
+        val_masks =  np.concatenate(val_masks, axis=0)
+
+
+        if save_dir is not None:
+            save_samples(Path(save_dir) / 'train' , train_images, train_masks)
+            save_samples(Path(save_dir) / 'val', val_images, val_masks)
+
+        return [train_images, train_masks], [val_images, val_masks]
+
     # return [tf.data.Dataset.from_tensor_slices(_build_samples(train_count, train_x, train_y, **kwargs)),
     #         tf.data.Dataset.from_tensor_slices(_build_samples(train_count, train_x, train_y, **kwargs))]
     #
