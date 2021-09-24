6917d8bc60e88bb30e7416f459401bd3ee3726a9
Marek Grochowski <grochu@is.umk.pl>
2021-09-24 23:06:14 +0200

    Wotking training script with keras-segmentation

--- train_ks.py
+++ train_ks.py
@@ -1,37 +1,60 @@
 from keras_segmentation_mod.models.unet import vgg_unet
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard
 from datetime import datetime
+from pathlib import Path
+import tensorflow as tf
+
+physical_devices = tf.config.list_physical_devices('GPU')
+print(physical_devices)
+#
+# from tensorflow.compat.v1 import ConfigProto
+# from tensorflow.compat.v1 import InteractiveSession
+#
+#
+# config = ConfigProto()
+# config.gpu_options.allow_growth = True
+#     # session = InteractiveSession(config=config)
+#
+# session = InteractiveSession(config=config)
+
+
 
 model = vgg_unet(n_classes=3, input_height=480, input_width=480, channels=1 )
 
 # When using custom callbacks, the default checkpoint saver is removed
 
-timestamp = str(datetime.now()).replace(' ', '_')
+timestamp = str(datetime.now()).replace(' ', '_').replace(':','')
 
-log_dir = 'logs/' + model.model_name + '_' + timestamp + '/'
+# log_dir = 'logs/' + model.model_name + '_' + timestamp + '/'
+log_dir = 'logs/' + model.model_name + "_" + timestamp
 print('log_dir', log_dir)
+p = Path(log_dir)
+
+Path.mkdir(p, parents=True)
 
 callbacks = [
     ModelCheckpoint(
-                filepath="checkpoints_" + timestamp + '/' + model.model_name + ".{epoch:05d}",
+                filepath=log_dir + '/checkpoints/' + model.model_name + ".{epoch:05d}",
                 save_weights_only=True,
                 verbose=True
             ),
-    EarlyStopping(),
+    EarlyStopping(verbose=2, patience=3, monitor='val_accuracy', mode='max', restore_best_weights=True),  # cause error at early stopping event !
     TensorBoard(log_dir=log_dir, histogram_freq=1, write_graph=True, write_images=False, update_freq=5)
 ]
 
+# callbacks = None
 model.train(
-    train_images =  "../dane/coprates_1M_resize_0.2_480x480_overlap_120/images/",
-    train_annotations = "../dane/coprates_1M_resize_0.2_480x480_overlap_120/annotations/",
-    batch_size = 2,
-    steps_per_epoch=50,
-    checkpoints_path = "checkpoints/" + model.model_name  , epochs=5,
-    val_images = "../dane/hydrotes_1M_resize_0.2_480x480_overlap_120/images/",
-    val_annotations = "../dane/hydrotes_1M_resize_0.2_480x480_overlap_120/annotations/",
+    train_images =  "data/mars_data_20210923/train/images/",
+    train_annotations = "data/mars_data_20210923/train/annotations/",
+    batch_size = 20,
+    steps_per_epoch = 10,
+    checkpoints_path = "checkpoints/" + model.model_name  ,
+    epochs=20,
+    val_images = "data/mars_data_20210923/val/images/",
+    val_annotations = "data/mars_data_20210923/val/annotations",
     validate=True,
-    val_steps_per_epoch=50,
+    val_steps_per_epoch=2,
     callbacks=callbacks,
     read_image_type=0,
-#     do_augment=True,
+    do_augment=True,
 )
